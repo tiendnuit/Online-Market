0b98a9e614794c0c9bcb43265f68c8b89e15a7dc
wondyfraw <wondyfraw@users.noreply.github.com>
2020-07-14 00:15:39 +0200

    Change seller product url(from /product to /seller/product), edit the thymeleaf and Cotroller

---
+++ src/main/java/edu/miu/cs545/group5/onlinemarket/config/ImageUtil.java
@@ -0,0 +1,9 @@
+package edu.miu.cs545.group5.onlinemarket.config;
+
+import java.util.Base64;
+
+public class ImageUtil {
+    public String getImgData(byte[] byteData) {
+        return Base64.getMimeEncoder().encodeToString(byteData);
+    }
+}
