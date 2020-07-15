7fb8bde3afda4d89e312492e79a31b65f4e64e48
Tien Doan <tiendnuit@gmail.com>
2020-07-15 07:41:32 +0700

    Update mockup data

--- src/main/java/edu/miu/cs545/group5/onlinemarket/config/DataLoader.java
+++ src/main/java/edu/miu/cs545/group5/onlinemarket/config/DataLoader.java
@@ -100,24 +100,46 @@ public class DataLoader implements ApplicationRunner {
         categoryRepository.saveAll(Arrays.asList(computer, food, book, shoe));
 
         //Product
-        Product product1 = new Product("HP", 100.0,
-                100, "HP computer",
-                "hp.png", computer, seller1);
+        Product product1 = new Product("Apple 13\" MacBook Air Core i5 CPU, 8GB RAM (2017 Model 128GB)", 955.67,
+                100, "1.8 GHz dual-core Intel Core i5 Processor\n" +
+                "Intel HD Graphics 6000\n" +
+                "Fast SSD Storage\n" +
+                "8GB memory\n",
+                "macbook-air-13.jpg", computer, seller4);
 
-        Product product2 = new Product("HP 2", 100.0,
-                100, "HP computer",
-                "hp2.jpg", computer, seller2);
+        Product product2 = new Product("New Apple MacBook Pro (16-Inch, 16GB RAM, 512GB Storage) - Space Gray", 2399.00,
+                100, "Ninth-generation 6-Core Intel Core i7 Processor\n" +
+                "Stunning 16-inch Retina Display with True Tone technology\n" +
+                "Touch Bar and Touch ID\n" +
+                "Amd Radeon Pro 5300M Graphics with GDDR6 memory\n",
+                "macbook-pro-16.jpg", computer, seller4);
 
-        Product product3 = new Product("Java 1", 100.0,
-                100, "Java Programming",
-                "java.jpg", book, seller3);
+        Product product3 = new Product("HP 21.5-Inch All-in-One Computer, AMD A4-9125, ", 421.58,
+                100, "The essential home computer: With an AMD processor and 4 GB of RAM, your family can seamlessly go from sending work emails to uploading vacation photos with ease\n" +
+                "A centerpiece for ",
+                "hp-21.jpg", computer, seller1);
 
-        Product product4 = new Product("Java 2", 100.0,
-                100, "Java Programming 2",
-                "java2.jpg", book, seller4);
+        Product product4 = new Product("Java 2", 10.0,
+                100, "Java Programming 1, The fact is your brain craves novelty. It's constantly searching, scanning, waiting for something unusual to happen.",
+                "java2.jpg", book, seller2);
+
+        Product product5 = new Product("Head First Java", 20.0,
+                100, "Learning a complex new language is no easy task especially when it's an object-oriented computer programming language like java. You might think the problo take in the dry, ",
+                "hf-java.jpg", book, seller3);
+
+        Product product6 = new Product("Frito-Lay Flavor Mix, 28 Count", 9.98,
+                100, "Frito-Lay Fun Times Mix Variety Pack provides the perfect portion size and variety to keep your entire family happy. From the pantry to the lunch box, all you have to do is grab a pack and go!  .",
+                "frito.jpg", food, seller1);
+
+        Product product7 = new Product("Fiber One 70 Calories Brownies, Chocolate Fudge, 18 Ct Mega Pack, 16 Oz", 6.76,
+                100, "New recipe! Don't compromise on great taste. Fiber One Chocolate Fudge Brownies have 70 calories, 5 net carbs and 2 grams of sugar.\n" +
+                "\n" +
+                "TASTY INDULGENCES: Guilt-free 70 calorie chocolate fudge brownie\n" +
+                "FUDGE BROWNIE: a tasty combination of rich",
+                "fiber.jpg", food, seller1);
 
         //productRepository.save(product1);
-        productRepository.saveAll(Arrays.asList(product1, product2, product3, product4));
+        productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5, product6, product7));
 
 
     }
